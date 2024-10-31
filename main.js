1b974ed0982849d008d42acf855d0d149a9209af
ivandalechenko <ivandalechenko@gmail.com>
2024-10-31 18:34:50 +0200

    added all

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import './style.scss';
 
 
-
+const ca = '12345678';
+document.getElementById('ca_val').innerHTML = ca
+document.getElementById('ca').onclick = (ca) => {
+    navigator.clipboard.writeText(ca)
+        .then(() => {
+            alert("CA Successfully copied!")
+        })
+}
